3c765412a8a16468da539f0db76201ae1299f3e9
Luis Alberto Bolaños Salazar <68343185+albertbolanoss@users.noreply.github.com>
2024-03-23 12:06:30 -0500

    Refactor controller

--- src/main/java/com/perficient/shoppingcart/application/api/controllers/CartsController.java
+++ src/main/java/com/perficient/shoppingcart/application/api/controllers/CartsController.java
@@ -1,24 +1,22 @@
 package com.perficient.shoppingcart.application.api.controllers;
 
+import com.perficient.shoppingcart.application.api.controller.ApiUtil;
 import com.perficient.shoppingcart.application.api.controller.CartApi;
-import com.perficient.shoppingcart.application.api.model.Item;
-import io.swagger.v3.oas.annotations.Parameter;
-import jakarta.validation.Valid;
+import com.perficient.shoppingcart.application.api.model.Cart;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 public class CartsController implements CartApi {
-    public ResponseEntity<List<Item>> addCartItemsByCustomerId(
-            @Parameter(name = "customerId", description = "Customer Identifier", required = true)
-            @PathVariable("customerId") String customerId,
-            @Parameter(name = "Item", description = "Item object")
-            @Valid @RequestBody(required = false) Item item
-    ) throws Exception {
-        return null;
+    @Override
+    public ResponseEntity<List<Cart>> getCartByCustomerId(String customerId) throws Exception {
+        return ok(Collections.EMPTY_LIST);
     }
 }
